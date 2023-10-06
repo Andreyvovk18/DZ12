c81b24c07421f6368c521cc1f93bbe36bd2dc97b
Andrey <Andreyvovk18@gmail.com>
2023-10-06 10:57:31 +0300

    1

---
+++ Main.py
@@ -0,0 +1,123 @@
+from collections import UserDict
+from datetime import datetime
+import pickle
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value):
+        if not value:
+            raise ValueError("Name cannot be empty")
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError("Phone number must be 10 digits")
+        super().__init__(value)
+
+class Birthday(Field):
+    
+    def __init__(self, value):
+        try:
+            datetime.strptime(value, "%Y-%m-%d")
+        except ValueError:
+            raise ValueError("Invalid date format for Birthday")
+        super().__init__(value)
+        
+class Record:
+    
+    def __init__(self, name, birthday=None):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = Birthday(birthday) if birthday else None
+        
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for phone in self.phones:
+            if phone.value == old_phone:
+                phone.value = new_phone
+                return
+        raise ValueError(f"Phone number '{old_phone}' not found")
+
+    def find_phone(self, phone):
+        phones_found = [p for p in self.phones if p.value == phone]
+        return phones_found[0] if phones_found else None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
+
+    def days_to_birthday(self):
+        if not self.birthday:
+            return -1
+
+        today = datetime.now().date()
+        next_birthday = datetime.strptime(self.birthday.value, "%Y-%m-%d").date().replace(year=today.year)
+        if today > next_birthday:
+            next_birthday = next_birthday.replace(year=today.year + 1)
+
+        days_until_birthday = (next_birthday - today).days
+        return days_until_birthday
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        if name in self.data:
+            return self.data[name]
+        else:
+            return None
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+    
+    
+    def __iter__(self):
+        self.current_record = 0
+        self.records = list(self.data.values())
+        return self
+
+    def __next__(self):
+        if self.current_record < len(self.records):
+            record = self.records[self.current_record]
+            self.current_record += 1
+            return record
+        else:
+            raise StopIteration
+    
+    def search(self, query):
+        results = []
+        for record in self.data.values():
+            if (
+                query.lower() in record.name.value.lower() or
+                any(query.lower() in phone.value for phone in record.phones)
+            ):
+                results.append(record)
+        return results        
+    def save_to_file(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(self.data, file)
+
+    def load_from_file(self, filename):
+        try:
+            with open(filename, 'rb') as file:
+                self.data = pickle.load(file)
+        except FileNotFoundError:
+            self.data = {}
+            
+            
+if __name__ == "__main__":
+    address_book = AddressBook()
